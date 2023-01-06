a0e7f6b5fab11166136f608c3268a63a57deb0ae
Alexis BERTIN <alexis.bertin@bts.sio>
2023-01-06 16:48:22 +0100

    dzd

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const fse = require('fs-extra');
 const zlib = require('zlib') ;
 
 function  sauvegarde (cheminDepart, cheminDestination){
-    let laDate =  new Date("YYYY-MM--DD"); 
+    let laDate =  new Date(); 
     try {
-        fse.copySync(cheminDepart, cheminDestination+'BUP-'+laDate+'.tgz', { overwrite: true|false })
+        fse.copySync(cheminDepart, cheminDestination+'BUP-'+laDate.now()+'.tgz', { overwrite: true|false })
         console.log('Réussite !')
       } catch (err) {
         console.error(err)
       }
 }
-
 
 sauvegarde("TEST","Arrive");
