e6719e15a450349a43c49abe81c2dd675550e995
Alexis Bertin <alex.bertin03@gmail.com>
2023-01-08 22:46:23 +0100

    ajout unzip et setTimer

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const fse = require('fs-extra');
-const zlib = require('zlib') ;
+const zipper = require("zip-local");
+const fs = require('fs');
 
-function  sauvegarde (cheminDepart, cheminDestination){
-    let laDate =  new Date(); 
+
+const dezip = (CheminDuFichierADeziper,cheminDestination) =>{
     try {
-        fse.copySync(cheminDepart, cheminDestination+'BUP-'+laDate.now()+'.tgz', { overwrite: true|false })
-        console.log('Réussite !')
+        zipper.sync.unzip(CheminDuFichierADeziper).save(cheminDestination);
+        console.log('Dezipage Réussi !')
       } catch (err) {
         console.error(err)
       }
 }
 
-sauvegarde("TEST","Arrive");
+const sauvegarde = (cheminDepart,cheminDestination) =>{
+  let maDate1 =  new Date();
+  let maDate2 = maDate1.getFullYear()+"-"+maDate1.getDate()+"-"+maDate1.getMonth()+1;
+  try {
+      zipper.sync.zip(cheminDepart).compress().save('./'+cheminDestination+'/BUP-'+maDate2+'.tar.gz');
+      console.log('Sauvegarde Réussite !')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+const miniteurSauvegarde = (cheminDepart,cheminDestination) => {
+  setTimeout(() => {
+    sauvegarde(cheminDepart,cheminDestination)
+  },5000);
+}
+
+
+miniteurSauvegarde("Depart","Arrive");
+//dezip("Arrive/BUP-2023-8-01.tar.gz","Unzip");
